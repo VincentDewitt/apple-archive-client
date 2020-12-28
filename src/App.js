28cc3abc57abad19d3c8496453c517aea7188989
Vincent Cataldo <vincentcataldo@me.com>
2020-12-28 14:14:24 -0800

    intial wireframe deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <div class="container">
+		<div class="SideBar">
+			<nav class="fullnav" role="navigation">
+        <ul>
+          <li>1997</li>
+          <a href="https://applearchivewireframe.vincentcataldo.repl.co/productpage.html">
+           <li>iMac</li>
+          </a>
+        </ul>
+    </nav>
+    </div>
+    <div class="content">
+    <main role="main">
+      <a href='https://applearchivewireframe.vincentcataldo.repl.co/searchpage.html'>
+      <button class="fullsearchbutton">
+      Search
+      </button>
+      </a>
+      <header role="banner">
+        <h1>AppleArchive</h1>
       </header>
+      <section>
+        <header>
+            <h2>What is AppleArchive?</h2>
+        </header>
+        <p>
+          Place Holder text on what the app is. 
+          Praesent sagittis a mi sit amet dictum. Donec orci nibh, dignissim in leo et, congue semper mauris. Donec elit lacus, dictum et placerat eget, rhoncus sodales erat. Curabitur sit amet placerat neque, a tempus mi. Suspendisse a tempus dolor. Nullam porttitor nisi sed justo dictum consequat. Etiam sed congue felis.</p>
+      </section>
+      <section>
+        <header>
+            <h2>About the Developer</h2>
+            <p>Placeholder image and text about me</p>
+        </header>
+      </section>
+    </main>
+    </div>
+    </div>
+    <footer role="content-info">Footer</footer>
     </div>
   );
 }
